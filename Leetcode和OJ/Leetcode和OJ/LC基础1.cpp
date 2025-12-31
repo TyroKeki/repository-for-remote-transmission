8b303bc6966a05051a4c794273bf67717f8d6592
TyroKeki <1372660468@qq.com>
2025-12-31 12:06:28 +0800

    LeetCode today

--- Leetcode和OJ/Leetcode和OJ/LC基础1.cpp
+++ Leetcode和OJ/Leetcode和OJ/LC基础1.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
+/** 温度转换
  * Note: The returned array must be malloced, assume caller calls free().
  */
 double* convertTemperature(double celsius, int* returnSize) {
@@ -11,7 +11,7 @@ double* convertTemperature(double celsius, int* returnSize) {
     *returnSize = 2;
     return res;
 }
-
+//最小偶倍数
 int smallestEvenMultiple(int n) {
     if (n % 2 == 0) {
         return n;
@@ -19,4 +19,35 @@ int smallestEvenMultiple(int n) {
     else {
         return n * 2;
     }
+}
+
+// 二叉树数据读取1
+/*Definition for a binary tree node.*/
+  struct TreeNode {
+      int val;
+      struct TreeNode *left;
+      struct TreeNode *right;
+  };
+ 
+bool checkTree(struct TreeNode* root) {
+    if (root->left->val + root->right->val == root->val) return true;
+    else return false;
+}
+
+// 异或练习
+int xorOperation(int n, int start) {
+    int i, j, k;
+    int result;
+    result = start + 2 * 0;
+    for (i = 1; i < n; i++) {
+        result = result ^ (start + 2 * i);
+        //printf("%d", nums[i]);
+    }
+ 
+    return result;
+}
+
+int main() {
+    int res = xorOperation(4, 3);
+    printf("%d", res);
 }
